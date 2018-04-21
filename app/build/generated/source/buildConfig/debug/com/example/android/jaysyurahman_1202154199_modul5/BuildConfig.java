f15b7444a6d5fe8287705a92da6f5e51e9fbd427
Jaysyurahman <rahmanjaysyu@telkomuniversity.ac.id>
2018-04-21 22:35:46 +0700

    first time

---
+++ app/build/generated/source/buildConfig/debug/com/example/android/jaysyurahman_1202154199_modul5/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.android.jaysyurahman_1202154199_modul5;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.android.jaysyurahman_1202154199_modul5";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
